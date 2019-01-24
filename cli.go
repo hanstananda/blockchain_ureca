003e555066da1bb95f1fb7d99f9cd61f7879f5a5
hanstananda <hanstananda@gmail.com>
2019-01-24 12:24:49 +0800

    added cli

--- cli.go
+++ cli.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"strconv"
 )
 
 // CLI responsible for processing command line arguments
@@ -40,10 +39,6 @@ func (cli *CLI) printChain() {
 		fmt.Printf("Prev. hash: %x\n", block.PrevBlockHash)
 		fmt.Printf("Data: %s\n", block.Data)
 		fmt.Printf("Hash: %x\n", block.Hash)
-		pow := NewProofOfWork(block)
-		fmt.Printf("PoW: %s\n", strconv.FormatBool(pow.Validate()))
-		fmt.Println()
-
 		if len(block.PrevBlockHash) == 0 {
 			break
 		}
