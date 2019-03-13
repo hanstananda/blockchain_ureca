833b40c1c21798f7162ef61c39ac23666a257ca2
hanstananda <hanstananda@gmail.com>
2019-03-13 18:33:14 +0800

    added voting, still have some unknown errors in encoding

--- server.go
+++ server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"strings"
 	"bytes"
 	"encoding/gob"
 	"encoding/hex"
@@ -8,6 +9,7 @@ import (
 	"io"
 	"log"
 	"net"
+	"strconv"
 )
 
 const protocol = "udp"
@@ -15,16 +17,40 @@ const nodeVersion = 1
 const commandLength = 12
 const maxDatagramSize = 8192
 
+var numNodes = 3
+var selfID = ""
 var nodeAddress string
 var addrUDP = "224.0.0.1"
-var knownNodes = []string{"3000"}
+var NotaryNodes = []string{"3000"}
 var mempool = make(map[string]Transaction)
 
+func isNotary(nodeID string) bool{
+	for _, n := range NotaryNodes{
+		if nodeID == n{
+			fmt.Println("This is the notary node!")
+			return true
+		}
+	}
+	return false
+}
+
+type tally struct {
+	yes 		int
+	no 			int
+	nodes 		[]string
+}
 
 type tx struct {
 	AddFrom     string
 	Transaction []byte
 }
+
+type vote struct {
+	AddFrom string
+	result    string
+}
+
+var votePool = make(map[string]tally)
 
 func commandToBytes(command string) []byte {
 	var bytes [commandLength]byte
@@ -80,7 +106,6 @@ func sendTx(tnx *Transaction, target_group string) {
 	data := tx{nodeAddress, tnx.Serialize()}
 	payload := gobEncode(data)
 	request := append(commandToBytes("tx"), payload...)
-
 	sendData(request, target_group)
 }
 
@@ -88,6 +113,26 @@ func SendID(nodeID ,target_group string){
 	payload := commandToBytes(nodeID)
 	request := append(commandToBytes("syn"), payload...)
 	sendData(request, target_group)
+}
+
+func SendVote(nodeID, target_group string,ID []byte, result bool){
+	data := vote{nodeID+","+string(ID[:])+","+strconv.FormatBool(result), "abc" }
+	fmt.Println(data)
+	payload := gobEncode(vote{nodeID+","+string(ID[:]), strconv.FormatBool(result) })
+	fmt.Println(payload)
+	request := append(commandToBytes("vot"), payload...)
+	fmt.Println(request)
+	var buff bytes.Buffer
+	buff.Write(request[commandLength:])
+	dec := gob.NewDecoder(&buff)
+	var res vote
+	err := dec.Decode(&res)
+	if err != nil {
+		log.Panic(err)
+	}
+	fmt.Println(buff)
+	fmt.Println(res)
+	sendData(request,target_group)
 }
 
 func SendTxs(nodeID string,target_group string){
@@ -122,14 +167,18 @@ func SendTxs(nodeID string,target_group string){
 func StartServer(nodeID, portUDP string, h func(*net.UDPAddr, int, []byte, *Blockchain)) {
 	nodeAddress = fmt.Sprintf("localhost:%s", nodeID)
 	target_group := fmt.Sprintf("%s:%s",addrUDP,portUDP)
+	selfID = nodeID
 	addr, err := net.ResolveUDPAddr("udp", target_group) // currently will always connect to a udp port
 	if err != nil {
 		log.Panic(err)
 	}
 
-	if nodeID=="3000"	{
-		SendTxs(nodeID,target_group)
+	if isNotary(nodeID)==false{
+		SendVote(nodeID, target_group, []byte("test"), true)
 	}
+	//if nodeID=="3000"	{
+	//	SendTxs(nodeID,target_group)
+	//}
 
 	ln, err := net.ListenMulticastUDP(protocol,nil, addr)
 	if err != nil {
@@ -159,14 +208,62 @@ func handleConnection(conn *net.UDPAddr, n int, b []byte, bc *Blockchain) {
 	request := b
 	command := bytesToCommand(request[:commandLength])
 	fmt.Printf("Received %s command\n", command)
-	// log.Println(request)  // hard debug
+	log.Println(request)  // hard debug
 
+	if isNotary(selfID)==true{
+		switch command {
+		case "vt":
+			handleVote(request,bc)
+			return
+		}
+	}
 	switch command {
 	case "tx":
 		handleTx(request, bc)
 	default:
 		fmt.Println("Unknown command!")
 	}
+}
+
+func handleVote(request []byte, bc * Blockchain){
+	var buff bytes.Buffer
+	var payload vote
+
+	buff.Write(request[commandLength:])
+	dec := gob.NewDecoder(&buff)
+	err := dec.Decode(&payload)
+	if err != nil {
+		log.Panic(err)
+	}
+	fmt.Println(payload)
+	tmp := strings.Split(payload.AddFrom,",")
+	TxID := tmp[0]
+	result := tmp[1]
+	if val, ok := votePool[TxID]; ok {
+		if(result =="true"){
+			val.yes ++
+
+		}else {
+			val.no++
+		}
+		val.nodes = append(val.nodes, payload.AddFrom)
+		fmt.Println(result,val.yes,val.no,numNodes,val.nodes)
+		if val.yes> numNodes/2+1{
+			fmt.Printf("Transaction %s accepted!\n",TxID)
+		}
+	}	else{
+		// New payload
+		var val tally
+		if(result =="true"){
+			val.yes++
+		}		else{
+			val.no++
+		}
+		val.nodes = append(val.nodes, payload.AddFrom)
+		votePool[TxID]= val
+		fmt.Println(result,val.yes,val.no,numNodes,val.nodes)
+	}
+
 }
 
 func handleTx(request []byte, bc *Blockchain) {
--- transaction.go
+++ transaction.go
@@ -22,7 +22,7 @@ type Transaction struct {
 }
 
 // IsCoinbase checks whether the transaction is coinbase
-func (tx Transaction) IsCoinbase() bool {
+	func (tx Transaction) IsCoinbase() bool {
 	return len(tx.Vin) == 1 && len(tx.Vin[0].Txid) == 0 && tx.Vin[0].Vout == -1
 }
 
